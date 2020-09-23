6c5d5593505bac5e3914601cc85865b1ef9278cc
josh055 <adejoh382@gmail.com>
2020-09-23 07:56:17 +0100

    update code with new features

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,30 +1,37 @@
 import React, { Component } from 'react';   
 class Counter extends Component {
-    state ={
-        count:0,
-        tags: ['tag1', 'tag2', 'tag3','tag4']
-       
-    };
-    render() { 
-       // let classes = this.getBadgeClasses();
 
+ 
+
+    render() { 
+        
         return (
         <div>
-          
+            
+            <h4>{this.props.id}</h4>
+<span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+<button onClick={()=> this.props.onIncrement(this.props.counter)} className="btn btn-secondary mx-3">Increment</button>
+<button onClick={ () => this.props.onDelete(this.props.counter.id) } className="btn btn-danger">delete</button>
 
-       
-        <ul>
-        {this.state.tags.map(tag=><li key={tag}>{tag}</li>)}
-        </ul>
 
-        </div>);
-
-        
+        </div>);   
+    }
+    getBadgeClasses(){
+        let classes="badge m-2 badge-";
+        classes+=this.props.counter.value===0? "warning":"primary";
+        return classes;
     }
 
+    formatCount(){
+const {value }= this.props.counter;
+return value === 0? 'Zero':value;
+
+    }
 
 }
  
-
+/* <ul>
+        {this.state.tags.map(tag=><li key={tag}>{tag}</li>)}
+        </ul> */
 
 export default Counter;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+// stareless Function component
+
+    const NavBar=({totalCounters}) =>{
+
+      
+        return ( 
+            <nav className="navbar navbar-light bg-light">
+                <a className="navbar-brand" href="#">
+                Navbar{" "}
+                 <span className="badge bade-pill badge-secondary">
+                     {totalCounters}
+
+                 </span>
+                </a>
+            </nav>
+         );
+    };
+
+ 
+export default NavBar;
